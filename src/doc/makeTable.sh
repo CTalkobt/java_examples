09ef9f66397f5f23a35b2dd36ed27a1880967cdb
Craig Taylor <ctalkobt@ctalkobt.net>
2017-06-27 22:00:31 -0400

    Initial pass at script to generate documentation table for README.md

---
+++ src/doc/makeTable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ../main
+echo "| **Link** | **Usage** | **Description** |"
+echo "| ---- | ---- | ----------- |"
+for file in `find . -name "*.rmd" -print`; do
+	url=`echo ${file} | sed "s;^./;;" | sed "s;.rmd;.java;"`	
+	baseName=`basename $file | sed "s/.rmd//"`
+	simple=`cat $file | grep '^simple:' | sed 's;^simple: ;;'`
+	description=`cat $file | grep '^description: '| sed 's;^description: ;;'`
+    usage=`cat $file | grep "^usage: " | sed "s;^usage: ;;"`
+
+	echo "|"
+	echo "| [${simple:-$baseName}](src/main/${url}) | ${usage} |${description} |"
+	echo "|"
+done
+
