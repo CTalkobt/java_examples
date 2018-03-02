44ca1938e59385333b252c29b586d46f88efe767
Craig Taylor <ctalkobt@ctalkobt.net>
2018-03-02 03:58:57 -0500

    Fix license headers

--- junit/src/main/java/net/ctalkobt/example/junit/problems/privateMethods/PrivateMethod.java
+++ junit/src/main/java/net/ctalkobt/example/junit/problems/privateMethods/PrivateMethod.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * Copyright (C) 2018 Craig Taylor <ctalkobt@ctalkobt.net>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package net.ctalkobt.example.junit.problems.privateMethods;
 
 public class PrivateMethod {
